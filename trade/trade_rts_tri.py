666e451837c3908b383de7aaf88a2e81f3d68ffc
Aleksey Manin <Alkor135@gmail.com>
2025-09-25 06:51:04 +0300

    edit

--- trade/trade_rts_tri.py
+++ trade/trade_rts_tri.py
@@ -6,9 +6,9 @@ import logging
 # --- Конфигурация ---
 # Торгуемые инструменты и количество
 ticker_close = 'RIZ5'  # Инструмент для закрытия позиции
-quantity_close = '1'
+quantity_close = '2'
 ticker_open = 'RIZ5'  # Инструмент для открытия новой позиции
-quantity_open = '1'
+quantity_open = '2'
 ticker_lc = 'rts'  # Название инструмента в нижнем регистре для путей
 
 # Пути к файлам
