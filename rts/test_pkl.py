21f3309a8e4ec0b101977b598b808fece0cb6a1d
Aleksey Manin <Alkor135@gmail.com>
2025-11-28 11:44:26 +0300

    add README files for RSS scraper and sync script; update test script for better output

--- rts/test_pkl.py
+++ rts/test_pkl.py
@@ -9,11 +9,17 @@ with open(CACHE_FILE, "rb") as f:
     cache_data = pickle.load(f)
 
 print(f"Всего записей в кэше: {len(cache_data)}")
+print()
 
 # Превращаем в DataFrame
 df = pd.DataFrame(cache_data)
 
 print(df)
+print()
 print(df.columns)
+print()
 print(df['metadata'])
+print()
 print(df['metadata'][0])
+print()
+print(df['embedding'])
